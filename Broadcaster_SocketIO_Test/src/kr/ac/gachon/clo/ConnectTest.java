c028ec3a7d4bcc619d51605f2fa16325cc54c64f
seok0721 <seok0721@gmail.com>
2014-09-15 13:09:04 +0900

    Save broadcaster code

--- Broadcaster_SocketIO_Test/src/kr/ac/gachon/clo/ConnectTest.java
+++ Broadcaster_SocketIO_Test/src/kr/ac/gachon/clo/ConnectTest.java
@@ -1,18 +1,36 @@
 package kr.ac.gachon.clo;
 
+import io.socket.SocketIO;
+
 public class ConnectTest {
 
 	public static void main(String[] args) throws Exception {
-		AppRTCClient client = new AppRTCClient();
+		SocketIOClient client = new SocketIOClient();
+		client.setCallback(new SocketIOHandler());
+		client.setSocket(new SocketIO("http://211.189.20.193:10080/"));
 		client.connect();
-		client.login("seok0721@gmail.com", HashUtils.md5("0000").toUpperCase());
-		Thread.sleep(3000);
-		client.createRoom("asdf");
-		Thread.sleep(2000);
+
+		Thread.sleep(1000);
+
+		client.login("seok0721@gmail.com", "0000");
+
+		Thread.sleep(500);
+
+		client.create("news");
+
+		Thread.sleep(500);
+
+		client.offer("asdf");
+		Thread.sleep(500);
+
+		client.destroy();
+
+		Thread.sleep(500);
+
 		client.logout();
-		Thread.sleep(3000);
-		System.out.println(11111111);
+
+		Thread.sleep(1000);
+
 		client.disconnect();
-		System.out.println(2222);
 	}
 }
