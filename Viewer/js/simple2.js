c88ecc85060f29e04c464620cb83f38f0e02a6d8
seok0721 <seok0721@debian>
2014-11-20 15:53:37 +0900

    update

--- Viewer/js/simple2.js
+++ Viewer/js/simple2.js
@@ -49,38 +49,12 @@ $(function() {
       return;
     }
 
-    watch(channel, 1);
-  });
-
-  btnWatch2.click(function(e) {
-    var channel = txtChannel2.val();
-
-    if(channel.length == 0) {
-      alert('채널을 입력하세요.');
-      txtChannel1.focus();
-      return;
-    }
-
-    watch(channel, 2);
-  });
-
-  function watch(email, index) {
+    var email = channel;
     var conn = new RTCPeerConnection(ICE_SERVERS);  
     var socket = io.connect(NODE_SERVER);
-    var vidRemote = $('#vidRemote' + index)[0];
+    var vidRemote = $('#vidRemote1')[0];
 
-    conn.onicecandidate = iceCandidateListener;
-    conn.onaddstream = addStreamListener;
-
-    socket.on('connect', connect_handler);
-    socket.on('offer', offer_handler);
-
-    function addStreamListener(e) {
-      console.log(e.stream);
-      vidRemote.src = URL.createObjectURL(e.stream);
-    }
-
-    function iceCandidateListener(e) {
+    conn.onicecandidate = function(e) {
       if(!e.candidate) {
         return;
       }
@@ -88,7 +62,15 @@ $(function() {
       console.log(e);
 
       conn.addIceCandidate(new RTCIceCandidate(e.candidate));
-    }
+    };
+
+    conn.onaddstream = function(e) {
+      console.log(e.stream);
+      vidRemote.src = URL.createObjectURL(e.stream);
+    };
+
+    socket.on('connect', connect_handler);
+    socket.on('offer', offer_handler);
 
     function connect_handler() {
       console.log('connect, join channel:' + email);
@@ -145,6 +127,98 @@ $(function() {
         console.log(err);
       });
     }
-  }
+  });
+
+  btnWatch2.click(function(e) {
+    var channel = txtChannel2.val();
+
+    if(channel.length == 0) {
+      alert('채널을 입력하세요.');
+      txtChannel2.focus();
+      return;
+    }
+
+    var email = channel;
+    var conn = new RTCPeerConnection(ICE_SERVERS);
+    var socket = io.connect(NODE_SERVER);
+    var vidRemote = $('#vidRemote2')[0];
+
+    conn.onicecandidate = function(e) {
+      if(!e.candidate) {
+        return;
+      }
+
+      console.log(e);
+
+      conn.addIceCandidate(new RTCIceCandidate(e.candidate));
+    };
+
+    conn.onaddstream = function(e) {
+      console.log(e.stream);
+      vidRemote.src = URL.createObjectURL(e.stream);
+    };
+
+    socket.on('connect', connect_handler);
+    socket.on('offer', offer_handler);
+    socket.on('error', function(e) {
+      console.log(e);
+    });
+
+    function connect_handler() {
+      console.log('connect, join channel:' + email);
+
+      socket.emit('join', {
+        'email': email
+      });
+    }
+
+    function offer_handler(data) {
+      var sdp = data.sdp;
+
+      // console.log('receive offer');
+      // console.log(sdp);
+
+      if(conn.remoteDescription) {
+        // console.log('have remote description');
+        return;
+      }
+
+      console.log('set remote description');
+
+      conn.setRemoteDescription(new RTCSessionDescription({
+        'type': 'offer',
+        'sdp': sdp
+      }), function() {
+        console.log('set remote description success.');
+
+        conn.createAnswer(function(desc) {
+          conn.setLocalDescription(desc, function() {
+            spin(checkIceGatheringState, afterCheckingIceGatheringState);
+
+            function checkIceGatheringState() {
+              return conn.iceGatheringState == 'complete';
+            }
+
+            function afterCheckingIceGatheringState() {
+              spin(checkLocalDescription, emitAnswer);
+            }
+
+            function checkLocalDescription() {
+              return conn.localDescription != null;
+            }
+
+            function emitAnswer() {
+              socket.emit('answer', {
+                'sdp': conn.localDescription.sdp
+              });
+            }
+          });
+        });
+      }, function(err) {
+        console.log('set remote description failure.');
+        console.log(err);
+      });
+    }
+  });
 });
 
